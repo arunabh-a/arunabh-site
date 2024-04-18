daea3921d33370bfca769c15fa40d842dbd7e796
Arunabh Bhattacharya <arunabh.nd@gmail.com>
2024-04-18 20:08:28 +0530

    Mobile Warning

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import About from './containers/About'
 import Contact from './containers/Contact'
 import Projects from './containers/Projects'
 import Footer from './containers/Footer'
+import MobileWarning from './components/mobile'
 
 const App = () => {
     return (
-        <div className='flex flex-col'>
+        <div className='app-container'>
             <Menu />
                 <Hero />
                 <About />
                 <Projects />
                 <Contact />
             <Footer />
+            <MobileWarning />
         </div>
     )
 }
---
+++ src/components/mobile.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const MobileWarning = () => {
+  return (
+    <div className="mobile-warning">
+      <p>Please open this site on a desktop or laptop for the best experience.</p>
+    </div>
+  );
+};
+
+export default MobileWarning;
